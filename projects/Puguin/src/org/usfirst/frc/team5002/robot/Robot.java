ede94afbe590402b9573adf1875c6ddb129ee75b
Nathan <dragonroboticsprogramming@gmail.com>
2016-01-15 16:09:23 -0600

    Added drivetrain; removed example subsystem

--- projects/Puguin/src/org/usfirst/frc/team5002/robot/Robot.java
+++ projects/Puguin/src/org/usfirst/frc/team5002/robot/Robot.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import org.usfirst.frc.team5002.robot.commands.ExampleCommand;
-import org.usfirst.frc.team5002.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -17,7 +16,7 @@ import org.usfirst.frc.team5002.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+//	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
