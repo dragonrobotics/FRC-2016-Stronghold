ae622d5499593ff4283f28283e69f523401313ba
Pugging <Potato>
2016-01-16 09:56:29 -0600

    Banana bread is good

--- projects/Puguin/src/org/usfirst/frc/team5002/robot/subsystems/Drivetrain.java
+++ projects/Puguin/src/org/usfirst/frc/team5002/robot/subsystems/Drivetrain.java
@@ -1,18 +1,41 @@
 package org.usfirst.frc.team5002.robot.subsystems;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Drivetrain extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	private CANTalon mc1, mc2, mc3, mc4, mc5, mc6;
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	/**
+	 * constructor for drivetrain initializes CANTalon stuff
+	 */
+	public Drivetrain() {
+
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
+	public void joystickDrive(Joystick stick) {
+
+	}
+
+	public void joystickFOCDrive(Joystick stick) {
+
+	}
+
+	public void autoDrive(double x, double y, double angle) {
+
+	}
+
+	public boolean isInPosition() {
+
+		return false;
+	}
 }
-
