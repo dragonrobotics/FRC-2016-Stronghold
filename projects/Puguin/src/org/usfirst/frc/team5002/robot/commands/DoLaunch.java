d56e865cd0dcb694b1502c080ddbbcad051b0e94
Sebastian Mobo <stmobo@gmail.com>
2016-01-18 10:36:22 -0600

    Add simple command for max-power launches

---
+++ projects/Puguin/src/org/usfirst/frc/team5002/robot/commands/DoLaunch.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team5002.robot.commands;
+
+import org.usfirst.frc.team5002.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DoLaunch extends Command {
+
+    public DoLaunch() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.launcher);
+    	requires(Robot.belt);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.belt.stop();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.launcher.run();
+    	Robot.belt.run();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
