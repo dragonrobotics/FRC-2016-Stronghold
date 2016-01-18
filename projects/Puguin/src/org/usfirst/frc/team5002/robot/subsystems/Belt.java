dc4912d59d98a97e96b3aaf153bb8c55c5fa61af
Sebastian Mobo <stmobo@gmail.com>
2016-01-18 10:23:58 -0600

    Add launcher and belt subsystems

---
+++ projects/Puguin/src/org/usfirst/frc/team5002/robot/subsystems/Belt.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5002.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Belt extends Subsystem {
+	private CANTalon leftBelt, rightBelt;
+    
+    public Belt() {
+    	leftBelt = new CANTalon(3); /* TODO: Replace this with the actual motor id */
+    	rightBelt = new CANTalon(4);
+    	
+    	leftBelt.changeControlMode(TalonControlMode.PercentVbus);
+    	rightBelt.changeControlMode(TalonControlMode.PercentVbus);
+    }
+    
+    public void run() {
+    	leftBelt.set(1.0);
+    	rightBelt.set(-1.0);
+    }
+    
+    public void run(double percentSpeed) {
+    	leftBelt.set(percentSpeed);
+    	rightBelt.set(percentSpeed);
+    }
+    
+    public void stop() {
+    	leftBelt.set(0);
+    	rightBelt.set(0);
+    }
+	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
---
+++ projects/Puguin/src/org/usfirst/frc/team5002/robot/subsystems/Launcher.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5002.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Launcher extends Subsystem {
+    private CANTalon leftLaunchWheel, rightLaunchWheel;
+    
+    public Launcher() {
+    	leftLaunchWheel = new CANTalon(2); /* TODO: Replace this with the actual motor id */
+    	rightLaunchWheel = new CANTalon(1);
+    	
+    	leftLaunchWheel.changeControlMode(TalonControlMode.PercentVbus);
+    	rightLaunchWheel.changeControlMode(TalonControlMode.PercentVbus);
+    }
+    
+    public void run() {
+    	leftLaunchWheel.set(1.0);
+    	rightLaunchWheel.set(-1.0);
+    }
+    
+    public void run(double speed) {
+    	leftLaunchWheel.set(speed);
+    	rightLaunchWheel.set(-speed);
+    }
+    
+    public void stop() {
+    	leftLaunchWheel.set(0);
+    	rightLaunchWheel.set(0);
+    }
+	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
