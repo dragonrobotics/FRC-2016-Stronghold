f781436b1c7d234e88da8c8fd468fee882bd9a76
Sebastian Mobo <stmobo@users.noreply.github.com>
2016-02-10 09:25:52 -0600

    Update NetworkMessage interface to match nbstream
    See lib5002 for the CPP-side interface.
    See stmobo/lib5002@ea6b237 (or release 3) for more information.

--- projects/Puguin/src/org/usfirst/frc/team5002/robot/subsystems/network/NetworkMessage.java
+++ projects/Puguin/src/org/usfirst/frc/team5002/robot/subsystems/network/NetworkMessage.java
@@ -10,6 +10,51 @@ public abstract class NetworkMessage {
 	public Class responseType = null;
 	public char msgType = 0;
 	
+	public String getLenString(ObjectInputStream in) throws IOException {
+		short len = in.readShort();
+		StringBuilder str = new StringBuilder();
+		for(int i=0;i<len;i++) {
+			int c = in.readUnsignedByte();
+			if(c == 0)
+				break;
+			str.append(Character.toString((char) c));
+		}
+		return str.toString();
+	}
+	
+	public String getNullTermString(ObjectInputStream in) throws IOException {
+		StringBuilder str = new StringBuilder();
+		while(true) {
+			int c = in.readUnsignedByte();
+			if(c == 0)
+				break;
+			str.append(Character.toString((char) c));
+		}
+		return str.toString();
+	}
+	
+	public void putLenString(ObjectOutputStream out, String str) throws IOException {
+		out.writeShort(str.length());
+		for(int i=0;i<str.length();i++) {
+			out.writeByte((byte)str.charAt(i));
+		}
+	}
+	
+	public void putNullTermString(ObjectOutputStream out, String str) throws IOException {
+		for(int i=0;i<str.length();i++) {
+			out.writeByte((byte)str.charAt(i));
+		}
+		out.writeByte(0);
+	}
+	
+	public double getDouble(ObjectInputStream in) throws IOException {
+		return Double.parseDouble(this.getLenString());
+	}
+	
+	public void putDouble(ObjectOutputStream out, double d) throws IOException {
+		this.putLenString(Double.toString(d));
+	}
+	
 	abstract public void writeObjectTo(ObjectOutputStream out) throws IOException;
 	abstract public void readObjectFrom(ObjectInputStream in) throws IOException;
 }
